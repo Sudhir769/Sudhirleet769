c758f1bbe1b1553887e18211c1a40ccce461e2ee
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-07-05 15:34:38 +0530

    Added solution - LeetHub

---
+++ Tree from Postorder and Inorder - GFG/tree-from-postorder-and-inorder.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+Node* buildTree(int in[], int post[], int n);
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Node* root = buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+//Function to return a tree created from postorder and inoreder traversals
+int findposition(int in[],int inorderstart,int inorderend,int element,int n)
+{
+    for(int i=inorderstart;i<=inorderend;i++)
+    {
+        if(in[i]==element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+Node* solve(int in[], int post[],int &index, int is, int ie, int n){
+    if( (index < 0) || (is>ie)){
+        return NULL;
+    }
+    int element = post[index--];
+    Node* root = new Node(element);
+    
+    int position = findposition(in, is,ie, element, n);
+    
+    root-> right = solve(in, post, index, position + 1, ie, n);
+    root-> left = solve(in, post, index, is, position -1, n);
+    
+    return root;
+    
+}
+
+Node *buildTree(int in[], int post[], int n) {
+    int postOrderIndex = n-1;
+    return solve(in, post, postOrderIndex, 0, n-1, n);
+}
