e93fe1ec10d69d4590da7f04fe081d18123a7663
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-10-23 14:55:59 +0530

    Added solution - LeetHub

---
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    vector<int>v(arr, arr+n);
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<i;j++){
+	            if(arr[j]<arr[i]){
+	                v[i] = max(v[i], v[j]+arr[i]);
+	            }
+	        }
+	    }
+	    return *max_element(v.begin(), v.end());
+	}  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
