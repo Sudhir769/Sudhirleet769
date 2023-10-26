1c1b74b683d24fd92edbdd25442263eb8134152e
Sudhir Maurya <sudhirmaurya769@gmail.com>
2023-10-26 15:36:46 +0530

    Added solution - LeetHub

--- Minimum Operations - GFG/minimum-operations.cpp
+++ Minimum Operations - GFG/minimum-operations.cpp
@@ -10,12 +10,12 @@ class Solution
     {
         int cnt=0;
         while(n>0){
-            if(n%2==0){
-                n/=2;
-                cnt++;
+            if(n&1){
+                n--;
             }else{
-                n--;cnt++;
+                n/=2;
             }
+            cnt++;
         }
         return cnt;
     }
