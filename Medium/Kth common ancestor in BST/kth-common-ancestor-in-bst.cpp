cadb2fbf5a0d05bafacb53edc7b79a5a604a0705
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-04-04 00:11:30 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kth common ancestor in BST/kth-common-ancestor-in-bst.cpp
+++ Medium/Kth common ancestor in BST/kth-common-ancestor-in-bst.cpp
@@ -98,68 +98,44 @@ struct Node
 class Solution
 {
     public:
-    int ans=-1;
-    int flag=0;
     
-    Node* lowest(Node* root, int x, int y){
-        if(root == NULL) return NULL;
-        
-        if(root->data == x) return root;
-        if(root->data == y) return root;
-        
-        Node* leftAns =  lowest(root->left, x, y);
-        Node* rightAns = lowest(root->right, x, y);
-        
-        if(leftAns == NULL and rightAns == NULL) return NULL;
-        else if(leftAns != NULL and rightAns == NULL) return leftAns;
-        else if(leftAns == NULL and rightAns != NULL) return rightAns;
-        else return root;
-    }
-    
-    bool solve(Node* root, Node* lca, int &k){
-        if(root == NULL) return false;
-        
-        if(root->data == lca->data){
-            flag = true;
-            k--;
-            if(k==0){
-                ans = root->data;
-                flag = false;
-            }
-            return 1;
-        }
-        
-        int l = solve(root->left, lca, k);
-        if(l){
-            if(flag){
-                k--;
-                if(k==0){
-                    ans = root->data;
-                    flag = false;
-                }
-            }
-            return l;
-        }
-        
-        int r = solve(root->right, lca, k);
-        if(r){
-            if(flag){
-                k--;
-                if(k==0){
-                    ans = root->data;
-                    flag = false;
-                }
-            }
-            return r;
-        }
-    }
-    
-    int kthCommonAncestor(Node *root, int k, int x, int y)
+    /*You are required to complete below function */
+    int kthCommonAncestor(Node *root, int k,int x, int y)
     {
-        Node* lca = lowest(root, x, y);
-        solve(root, lca, k);
+        int total = 1;
+        Node* temp = root;
         
-        return ans;
+        while(root){
+            if(root->data > x and root->data > y){
+                root = root->left;
+                total++;
+            }else if(root->data < x and root->data < y){
+                root = root->right;
+                total++;
+            }else{
+                break;
+            }
+        }
+        
+        total -= k;
+        total++;
+        int c=1;
+        root=temp;
+        
+        while(root){
+            if(c == total) return root->data;
+            if(root->data > x and root->data > y){
+                root = root->left;
+                c++;
+            }else if(root->data < x and root->data < y){
+                root = root->right;
+                c++;
+            }else{
+                break;
+            }
+        }
+        return -1;
+        
     }
 };
 
