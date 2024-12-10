9c2b9cce3a5d07503a16dd5185f47e95874b03e3
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-12-10 20:11:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Non-overlapping Intervals/nonoverlapping-intervals.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minRemoval(vector<vector<int>> &intervals) {
+        sort(intervals.begin(), intervals.end());
+        int cnt = 0;
+        
+        for(int i=intervals.size()-1; i>0; i--){
+            if(intervals[i][0] < intervals[i-1][1]){
+                cnt++;
+                intervals.erase(intervals.begin()+i-1);
+            }
+        }
+        return cnt;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<vector<int>> intervals(N, vector<int>(2));
+        for (int i = 0; i < N; i++) {
+            cin >> intervals[i][0] >> intervals[i][1];
+        }
+        Solution obj;
+        cout << obj.minRemoval(intervals) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
