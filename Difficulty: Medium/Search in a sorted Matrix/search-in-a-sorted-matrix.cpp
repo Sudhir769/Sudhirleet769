533041e5f04e3131b89424f381eee2e0e6704f12
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-12-24 17:16:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
@@ -9,19 +9,19 @@ using namespace std;
 class Solution {
   public:
     // Function to search a given number in row-column sorted matrix.
-    bool searchMatrix(vector<vector<int> > &mat, int x) {
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
         int n = mat.size();
         int m = mat[0].size();
         
-        int i=0, j=m-1;
-        while(i < n and j >= 0){
-            if(mat[i][j] == x){
-                    return true;
-                }else if(mat[i][j] < x){
-                    i++;
-                }else{
-                    j--;
-                }
+        int low = 0, high = m-1;
+        while(low < n and high >= 0){
+            if(mat[low][high] == x){
+                return true;
+            }else if(mat[low][high] < x){
+                low++;
+            }else{
+                high--;
+            }
         }
         return false;
     }
